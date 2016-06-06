49ed34a3980231eb9b82d4d7e835766f57f5cf51
David Wiesner <david@apphoria.com>
2016-06-06 21:54:59 +0200

    use glide

--- src/com/owncloud/android/utils/glide/OCFileUrlLoader.java
+++ src/com/owncloud/android/utils/glide/OCFileUrlLoader.java
@@ -8,8 +8,15 @@ import com.bumptech.glide.load.data.DataFetcher;
 import com.bumptech.glide.load.model.GenericLoaderFactory;
 import com.bumptech.glide.load.model.ModelLoader;
 import com.bumptech.glide.load.model.ModelLoaderFactory;
+import com.owncloud.android.MainApp;
 import com.owncloud.android.authentication.AccountUtils;
+import com.owncloud.android.datamodel.FileDataStorageManager;
 import com.owncloud.android.datamodel.OCFile;
+import com.owncloud.android.lib.common.OwnCloudAccount;
+import com.owncloud.android.lib.common.OwnCloudClient;
+import com.owncloud.android.lib.common.OwnCloudClientManagerFactory;
+import com.owncloud.android.lib.common.utils.Log_OC;
+import com.owncloud.android.ui.fragment.FileFragment;
 
 import java.io.InputStream;
 
@@ -18,25 +25,68 @@ import java.io.InputStream;
  */
 
 public class OCFileUrlLoader implements ModelLoader<OCFile, InputStream> {
-	@Override
+    private static final String TAG = OCFileUrlLoader.class.getSimpleName();
+    private OwnCloudClient client;
+    private final Account account;
+    private final FileDataStorageManager storageManager;
+
+    public OCFileUrlLoader(OwnCloudClient client, Account account, FileDataStorageManager storageManager) {
+
+        this.client = client;
+        this.account = account;
+        this.storageManager = storageManager;
+    }
+
+    @Override
 	public DataFetcher<InputStream> getResourceFetcher(OCFile model, int width, int height) {
-		return null;
+        if(client == null){
+            Log_OC.d(TAG,account.toString());
+            client = OCFileUrlLoader.Factory.getClient(this.account);
+        }
+		return new OCFileThumbStreamFetcher(this.client, this.account, this.storageManager, model);
 	}
 
 	public static class Factory implements ModelLoaderFactory<OCFile, InputStream> {
-		private final Account account;
+        private final Account account;
+        private final FileDataStorageManager storageManager;
+        private final OwnCloudClient client;
 
-		public Factory(Context context) {
-			this(AccountUtils.getCurrentOwnCloudAccount(context));
+        public Factory(Context context) {
+			this(AccountUtils.getCurrentOwnCloudAccount(context), getStorageManager(context));
 		}
 
-		public Factory(Account account) {
-			this.account = account;
-		}
-
-		@Override
-		public ModelLoader<OCFile, InputStream> build(Context context, GenericLoaderFactory factories) {
+		private static FileDataStorageManager getStorageManager(Context context) {
+			if(context instanceof FileFragment.ContainerActivity){
+				return ((FileFragment.ContainerActivity) context).getStorageManager();
+			}
 			return null;
+		}
+
+
+		public Factory(Account account, FileDataStorageManager storageManager) {
+            this(getClient(account), account, storageManager);
+		}
+
+        public Factory(OwnCloudClient client, Account account, FileDataStorageManager storageManager) {
+            this.client = client;
+            this.account = account;
+            this.storageManager = storageManager;
+        }
+
+        public static OwnCloudClient getClient(Account account) {
+            try {
+                return OwnCloudClientManagerFactory.getDefaultSingleton().
+                        getClientFor(new OwnCloudAccount(account,
+                                MainApp.getAppContext()), MainApp.getAppContext());
+            } catch (Exception e) {
+                Log_OC.e(Factory.class.getCanonicalName(), e.getMessage(), e);
+            }
+            return null;
+        }
+
+        @Override
+		public ModelLoader<OCFile, InputStream> build(Context context, GenericLoaderFactory factories) {
+            return new OCFileUrlLoader(this.client, this.account, this.storageManager);
 		}
 
 		@Override
